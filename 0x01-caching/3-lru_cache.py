7c230de1f4d52a24ef047941bb52c2f6792b8f89
123harri <harrietwanjiku90@gmail.com>
2024-07-23 12:35:29 -0700

    a

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-""" LIFOCache module
+""" LRUCache module
 """
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
-class LIFOCache(BaseCaching):
-    """ LIFOCache class
-    A caching system that implements the LIFO algorithm.
+class LRUCache(BaseCaching):
+    """ LRUCache class
+    A caching system that implements the
+    LRU (Least Recently Used) algorithm.
     Inherits from BaseCaching.
     """
 
@@ -15,7 +17,7 @@ class LIFOCache(BaseCaching):
         """ Initialize
         """
         super().__init__()
-        self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -23,16 +25,17 @@ class LIFOCache(BaseCaching):
         """
         if key is not None and item is not None:
             if key in self.cache_data:
-                self.order.remove(key)
+                self.cache_data.move_to_end(key)
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key = self.order.pop()
-                del self.cache_data[last_key]
-                print("DISCARD: {}".format(last_key))
+                first_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD: {}".format(first_key))
             self.cache_data[key] = item
-            self.order.append(key)
 
     def get(self, key):
         """ Get an item by key
         If key is None or if the key doesn't exist, return None.
         """
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
