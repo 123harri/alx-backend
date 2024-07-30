709f62e824174a81f59b9ff66ed7978c21ddeb92
123harri <harrietwanjiku90@gmail.com>
2024-07-30 19:57:02 +0000

    a

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
 """
-from flask_babel import Babel
+A Flask application with internationalization (i18n) support using
+Flask-Babel.
+
+This application supports English and French languages and uses UTC as
+the default timezone.
+"""
+
 from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
 
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """
+    Configuration class for Flask application.
+
+    Attributes:
+        LANGUAGES (list): Supported languages for the application.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the application.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the application.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -15,13 +27,20 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale()
-    """Retrieves the locale for a web page.
+def get_locale() -> str:
+    """
+    Determines the best match language for the request.
+
+    The function first checks if a 'locale' parameter is provided in the
+    URL query string. If so, it returns that locale if it's supported.
+    Otherwise, it uses the best match from the Accept-Language HTTP header.
+
+    Returns:
+        str: The locale identifier that best matches the request.
     """
     locale = request.args.get("locale")
     if locale in app.config['LANGUAGES']:
@@ -29,12 +48,18 @@ def get_locale()
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home() -> str:
+    """
+    Renders the home page.
+
+    The function renders the '4-index.html' template.
+
+    Returns:
+        str: The rendered HTML template.
     """
     return render_template('4-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
