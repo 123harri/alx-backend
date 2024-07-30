23ed24d8edf844b4b203fbeff4cedb86fe2fe565
123harri <harrietwanjiku90@gmail.com>
2024-07-30 19:49:03 +0000

    a

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A Basic Flask app with Babel for internationalization.
 """
-from flask_babel import Babel, _
+from flask_babel import Babel, gettext
 from flask import Flask, render_template, request
 
 
