6ea380d9c44a2160e2ead81d3546bdaf2971235f
123harri <harrietwanjiku90@gmail.com>
2024-07-30 17:52:56 +0000

    a

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,6 +6,7 @@ This module sets up a basic Flask application.
 from flask import Flask, render_template
 
 app: Flask = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -14,7 +15,7 @@ def index() -> str:
     This function renders the index.html template
     at the root URL.
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
